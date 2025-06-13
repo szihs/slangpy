75f9d252a6bccb7b8aec952f8aedb9ecdb28e41d
Tomas Davidovic <tdavidovic@nvidia.com>
2025-06-13 06:32:27 +0200

    Added simplified faster codepath for _set_matrix

--- src/sgl/device/shader_cursor.cpp
+++ src/sgl/device/shader_cursor.cpp
@@ -565,12 +565,15 @@ void ShaderCursor::_set_matrix(
     if (rows > 1) {
         size_t mat_stride = m_type_layout->getStride();
         size_t row_stride = mat_stride / rows;
-
-        size_t row_size = size / rows;
-        ShaderOffset offset = m_offset;
-        for (int row = 0; row < rows; ++row) {
-            m_shader_object->set_data(offset, reinterpret_cast<const uint8_t*>(data) + row * row_size, row_size);
-            offset.uniform_offset += narrow_cast<uint32_t>(row_stride);
+        if (mat_stride == size) {
+            m_shader_object->set_data(m_offset, data, size);
+        } else {
+            size_t row_size = size / rows;
+            ShaderOffset offset = m_offset;
+            for (int row = 0; row < rows; ++row) {
+                m_shader_object->set_data(offset, reinterpret_cast<const uint8_t*>(data) + row * row_size, row_size);
+                offset.uniform_offset += narrow_cast<uint32_t>(row_stride);
+            }
         }
     } else {
         m_shader_object->set_data(m_offset, data, size);
