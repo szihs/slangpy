7562a50b0c3bb998de25f5573f515ffe25800930
Simon Kallweit <64953474+skallweitNV@users.noreply.github.com>
2025-07-01 09:55:45 +0200

    Revert "Fix build problem when multiple VS toolsets are installed (#315)" (#319)
    This reverts commit 1a0aee7822ce77ccb359f6d946e505d102cd1820.

--- external/vcpkg-triplets/x64-windows-static-md-fix.cmake
+++ external/vcpkg-triplets/x64-windows-static-md-fix.cmake
@@ -1,25 +1,3 @@
-# Allow user to explicitly specify a toolset version via -DSGL_MSVC_TOOLSET_VERSION=...
-# If not specified, it falls back to the VCToolsVersion from the developer environment.
-if(DEFINED SGL_MSVC_TOOLSET_VERSION)
-    set(toolset_to_use ${SGL_MSVC_TOOLSET_VERSION})
-    message(STATUS "Triplet: Using user-specified toolset version '${toolset_to_use}'.")
-elseif(DEFINED ENV{VCToolsVersion})
-    set(toolset_to_use "$ENV{VCToolsVersion}")
-    message(STATUS "Triplet: Using toolset version from environment: '${toolset_to_use}'.")
-endif()
-
-if(DEFINED toolset_to_use)
-    # Set the detailed toolset version. This forces vcpkg to use the specific toolset.
-    set(VCPKG_PLATFORM_TOOLSET_VERSION "${toolset_to_use}")
-
-    # Also set the major toolset version, as vcpkg may require it to be present.
-    string(REGEX MATCH "^([0-9]+)\\.([0-9])" _match "${toolset_to_use}")
-    if(_match)
-        set(derived_toolset "v${CMAKE_MATCH_1}${CMAKE_MATCH_2}")
-        set(VCPKG_PLATFORM_TOOLSET "${derived_toolset}")
-    endif()
-endif()
-
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
