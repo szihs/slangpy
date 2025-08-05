4db922f204d72aec3627c8aff2feea6f259c2b7f
Simon Kallweit <64953474+skallweitNV@users.noreply.github.com>
2025-08-05 15:39:54 +0200

    Enable more testing on examples (#401)
    * update slang-rhi
    
    * enable more texture tests on CUDA
    
    * update samples

--- slangpy/tests/slangpy_tests/test_textures.py
+++ slangpy/tests/slangpy_tests/test_textures.py
@@ -314,10 +314,6 @@ def test_copy_mip_values_with_resource_views(
 ):
     if device_type == DeviceType.metal and type == TextureType.texture_1d and mips > 1:
         pytest.skip("1D textures with mip maps are not supported on Metal")
-    if device_type == DeviceType.cuda and mips > 1:
-        pytest.skip(
-            "Investigating with CUDA team whether its valid to write multiple mip levels without syncing"
-        )
 
     m = load_test_module(device_type)
     assert m is not None
@@ -363,10 +359,6 @@ def test_copy_mip_values_with_all_uav_resource_views(
 ):
     if device_type == DeviceType.metal and type == TextureType.texture_1d and mips > 1:
         pytest.skip("1D textures with mip maps are not supported on Metal")
-    if device_type == DeviceType.cuda and mips > 1:
-        pytest.skip(
-            "Investigating with CUDA team whether its valid to write multiple mip levels without syncing"
-        )
 
     m = load_test_module(device_type)
     assert m is not None
